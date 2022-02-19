0634b4041ca0ded928facb8bec643dd4295b788d
Aleksandra Bogachevskaya <Alexandra.Bogachevskaya@t-systems.com>
2022-02-19 12:41:38 +0300

    03-objects-arrays-intro-to-testing: createGetter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return obj => {
+    let value = obj;
+    path.split('.').forEach(property => value = value?.[property]);
+    return value;
+  };
 }
